fd76c57f811cf0ed2ebec8357dc780945b2fb431
nicholedlc <nichole@spark.re>
2018-03-21 08:23:55 -0700

    Modify startTime/elapsedTime logic

--- customDuplexStream.js
+++ customDuplexStream.js
@@ -5,20 +5,22 @@ const { Transform } = require('stream');
 // Create a duplex stream that consume line-separated text and outputs objects 
 // with keys for the elapsed time, total length in bytes, and total lines.
 
+let startTime;
+
 const transformStream = new Transform({
     readableObjectMode: true,
     writableObjectMode: true,
     transform(chunk, encoding, callback) {
         const totalLines = chunk.toString().trim().split("\n").length;
         const totalBytes = chunk.byteLength
-        const startTime = Date.now();
         const elapsedTime = Date.now() - startTime
-        this.push(JSON.stringify({ totalLines, totalBytes, startTime }))
+        this.push(JSON.stringify({ totalLines, totalBytes, elapsedTime }))
         callback();
     }
 })
 
 server.on('request', (req, res) => {
+    startTime = Date.now();
     const stream = fs.createReadStream('./big.file');
     stream
         .pipe(transformStream)
