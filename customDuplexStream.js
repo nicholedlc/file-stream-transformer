ee757a0b60ffe413e066ed842636ad8046660f22
Nichole <nichole@spark.re>
2018-03-21 14:04:19 -0700

    Create stream to generate summary report

--- customDuplexStream.js
+++ customDuplexStream.js
@@ -6,25 +6,56 @@ const { Transform } = require('stream');
 // with keys for the elapsed time, total length in bytes, and total lines.
 
 let startTime;
+let byteCount = [];
+let timeArray = [];
+
+
+const bytesPerSec = byteCountArray => { 
+    const totalBytes = byteCountArray.reduce((acc, curr) => acc + curr) 
+    const totalTime = timeArray[timeArray.length - 1] / 1000;
+    return totalBytes / totalTime
+};
+
+const countLines = chunk => { chunk.toString().trim().split("\n").length }
 
 const transformStream = new Transform({
-    readableObjectMode: true,
     writableObjectMode: true,
+
     transform(chunk, encoding, callback) {
-        const totalLines = chunk.toString().trim().split("\n").length;
-        const totalBytes = chunk.byteLength
+        const totalLines = countLines(chunk);
+        const byteLength = chunk.byteLength
         const elapsedTime = Date.now() - startTime
-        this.push(JSON.stringify({ totalLines, totalBytes, elapsedTime }))
+        this.push(JSON.stringify({ totalLines, byteLength, elapsedTime }))
         callback();
     }
 })
 
-server.on('request', (req, res) => {
+// Create a stream that takes these objects and outputs one-line 
+// summary reports (human-readable). The report should include 
+// the throughput rate of the input stream in bytes/sec.
+
+const summaryReport = new Transform({
+    readableObjectMode: true,
+    writeableObjectMode: true,
+
+    transform(chunk, encoding, callback) {
+        chunkObj = JSON.parse(chunk)
+        byteCount.push(+chunkObj.byteLength)
+        timeArray.push(+chunkObj.elapsedTime)
+        const byteRate = bytesPerSec(byteCount);
+        this.push(JSON.stringify({ byteRate }))
+        callback();
+    }
+})
+
+// server.on('request', (req, res) => {
+module.exports = () => {
     startTime = Date.now();
     const stream = fs.createReadStream('./big.file');
     stream
         .pipe(transformStream)
+        .pipe(summaryReport)
         .pipe(process.stdout)
-})
+}
 
-server.listen(8000);
+// server.listen(8000);
